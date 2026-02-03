bab46747e8661cb37a7eafb37a36500ee43f00ab
Patrick Page <patrick.page@csatlanta.com>
2026-02-03 15:16:03 -0500

    Initial Commit

---
+++ ConquestSupportApp/ConquestSupportAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConquestSupportAppApp.swift
+//  ConquestSupportApp
+//
+//  Created by Patrick Page on 2/3/26.
+//
+
+import SwiftUI
+
+@main
+struct ConquestSupportAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
